dedd97b81460e5f826e445890a49dc2756efe490
m441249833 <43653486+m441249833@users.noreply.github.com>
2018-11-26 21:21:32 -0500

    Create Random.cpp

---
+++ PlayerStrategy/Random.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Random.h"
+
+
+void Random::execute(Player* p) {
+	cout << "************************Random strategy executed.**************************" << endl;
+	cout << "Random Player " << p->getId() << " now own the following:" << endl;
+	p->showTerritory();
+	cout <<endl<< "------------------------------------------------" << endl;
+	p->reinforceRandom();
+	cout << "------------------------------------------------" << endl;
+	p->attackRandom();
+	p->fotifyRandom();
+	cout << "------------------------------------------------" << endl;
+
+}
